48dcabf43d9cc524a9c25fe514dc3a01405c9094
ChechiDev <chechidatadev@gmail.com>
2026-01-20 17:09:33 +0100

    Add: + Makefile + Function and comment

---
+++ cursus/libft/ft_putchar_fd.c
@@ -0,0 +1,22 @@
+/*
+ft_putchar_fd
+
+Envía el carácter 'c' al descriptor de archivo especificado por 'fd'.
+
+Escribe exactamente un byte en el file descriptor dado utilizando la
+función write(). No realiza ninguna gestión de errores ni validación
+del descriptor; se asume que 'fd' es válido según el contrato de la
+función.
+
+Parámetros:
+- c: carácter a escribir.
+- fd: descriptor de archivo sobre el que se escribe.
+
+Valor de retorno:
+- Ninguno.
+*/
+
+void ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
