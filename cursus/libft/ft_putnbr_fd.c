334ef328d087cc1eea96848797b5115519dcbd4e
ChechiDev <chechidatadev@gmail.com>
2026-01-20 17:48:48 +0100

    Add: + Testing main Mod: + Function optimization

--- cursus/libft/ft_putnbr_fd.c
+++ cursus/libft/ft_putnbr_fd.c
@@ -14,22 +14,35 @@ En caso de que 'n' sea INT_MIN, debe imprimirse correctamente.
 
 void	ft_putnbr_fd(int n, int fd)
 {
+	long	nb;
 	char	c;
-
-	if (n == -2147483648)
-	{
-		write(fd, "-2147483648", 11);
-		return;
-	}
-	if (n < 0)
+	
+	nb = (long)n;
+	if (nb < 0)
 	{
 		write(fd, "-", 1);
-		n = -n;
+		nb = -nb;
 	}
-	if (n >= 10)
+	if (nb >= 10)
 	{
-		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd((int)(nb / 10), fd);
 	}
-	c = (char)('0' + (n % 10));
+	c = (char)('0' + (nb % 10));
 	write(fd, &c, 1);
 }
+/*
+int	main(int argc, char **argv)
+{
+	int	n;
+
+	if (argc != 2)
+	{
+		printf("Use: %s <num>\n", argv[0]);
+		return (1);
+	}
+	n = ft_atoi(argv[1]);
+	ft_putnbr_fd(n, 1);
+	write(1, "\n", 1);
+	return (0);
+}
+*/
