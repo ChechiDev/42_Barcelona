26fa9435c13a6f342dd98c03ea3e663e7d61bdb9
ChechiDev <chechidatadev@gmail.com>
2026-05-04 13:36:05 +0200

    add(M02) Exercise 1 done

---
+++ cursus/python/M02/ex1/ft_raise_exception.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+
+def input_temperature(temp_str: str) -> int:
+    """ Function that converts str into int """
+    temp = int(temp_str)
+
+    if (temp < 0):
+        raise ValueError(f"{temp_str}°C is too cold for plants (min 0°C)")
+    elif (temp > 40):
+        raise ValueError(f"{temp_str}°C is too hot for plants (max 40°C)")
+    return temp
+
+
+def test_temperature() -> None:
+    """ test temperature typing """
+
+    print("Input data is '25'")
+    try:
+        temp = input_temperature("25")
+        print(f"Temperature is now {temp}°C\n")
+
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}\n")
+
+    print("Input data is 'abc'")
+    try:
+        temp2 = input_temperature("abc")
+        print(f"Temperature is now {temp2}°C\n")
+
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}\n")
+
+    print("Input data is '100'")
+    try:
+        temp3 = input_temperature("100")
+        print(f"Temperature is now {temp3}°C\n")
+
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}\n")
+
+    print("Input data is '-50'")
+    try:
+        temp4 = input_temperature("-50")
+        print(f"Temperature is now {temp4}°C\n")
+
+    except ValueError as e:
+        print(f"Caught input_temperature error: {e}\n")
+
+    print("All tests completed - program didn't crash!")
+
+
+def main():
+    print("=== Garden Temperature ===\n")
+    test_temperature()
+
+
+if __name__ == "__main__":
+    main()
